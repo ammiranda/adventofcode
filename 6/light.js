f49bff709f585dda336e4a64c15ca25c800f6395
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2015-12-06 11:46:12 -0800

    Created two classes for the two parts for day 6

--- 6/light.js
+++ 6/light.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var Lights = function() {
+var LightsOne = function() {
    this.lights = [];
    for (var i = 0; i < 1000; i++) {
       var arr = [];
@@ -11,8 +11,6 @@ var Lights = function() {
    }
 
    this.alter = function(options) {
-      var deltaX = options.x2 - options.x1;
-      var deltaY = options.y2 - options.y1;
       for (var i = options.x1; i <= options.x2; i++) {
          for (var k = options.y1; k <= options.y2; k++) {
             if (options.mode === 'on') {
@@ -32,6 +30,45 @@ var Lights = function() {
          for (var k = 0; k < this.lights[i].length; k++) {
             if (this.lights[i][k]) {
                onCount++;
+            }
+         }
+      }
+      return onCount;
+   }
+}
+
+var LightsTwo = function() {
+   this.lights = [];
+   for (var i = 0; i < 1000; i++) {
+      var arr = [];
+      for (var k = 0; k < 1000; k++) {
+         arr.push(0);
+      }
+      this.lights.push(arr);
+   }
+
+   this.alter = function(options) {
+      for (var i = options.x1; i <= options.x2; i++) {
+         for (var k = options.y1; k <= options.y2; k++) {
+            if (options.mode === 'on') {
+               this.lights[i][k] += 1;
+            } else if (options.mode === 'off') {
+               if (this.lights[i][k] > 0) {
+                  this.lights[i][k] -= 1;
+               }
+            } else if (options.mode === 'toggle') {
+               this.lights[i][k] += 2;
+            }
+         }
+      }
+   }
+
+   this.onCount = function() {
+      var onCount = 0;
+      for (var i = 0; i < this.lights.length; i++) {
+         for (var k = 0; k < this.lights[i].length; k++) {
+            if (this.lights[i][k]) {
+               onCount += this.lights[i][k];
             }
          }
       }
@@ -67,15 +104,18 @@ fs.readFile('input.txt', function(err, data) {
       return err;
    }
    
-   var lights = new Lights();
+   var lightsOne = new LightsOne();   
+   var lightsTwo = new LightsTwo();
 
    var lines = data.toString().split('\n');
    for (var i = 0; i < lines.length; i++) {
       var line = lines[i];
       var options = parseLine(line);
       if (options) {
-         lights.alter(parseLine(line));
+         lightsOne.alter(parseLine(line));
+         lightsTwo.alter(parseLine(line));
       }
    }
-   console.log(lights.onCount());
+   console.log(lightsOne.onCount());
+   console.log(lightsTwo.onCount());
 });
